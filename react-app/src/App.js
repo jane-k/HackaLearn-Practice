b654208d2f6ed8caf767eb97961434476a211264
jiin3909 <jiin3909@admin.com>
2021-08-11 06:31:16 +0900

    update App.js

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          HackaLearn Practice Work!
+          HackaLearn Practice Work
         </p>
         <a
           className="App-link"
